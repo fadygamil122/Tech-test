5815a1fa8694a687bb7be31c0a12e6dc9bc76e41
fadygamil <test@gmail.com>
2025-10-23 10:37:54 +0300

    add status field

--- script.js
+++ script.js
@@ -9,14 +9,21 @@ function loadData(lang) {
       lang === 'ar' ? `<h2 id="name-ar" class="name-ar">${data.name_ar}</h2>`: `<h2 id="name-ar" class="name-ar">${data.name_en}</h2>`;
       document.getElementById('department').textContent =
         lang === 'ar' ? data.department_ar : data.department_en;
+      document.getElementById('tech-status').textContent =
+        lang === 'ar' ? data.status_ar : data.status_en;
       document.getElementById('survey-btn').textContent =
         lang === 'ar' ? 'رأيك يهمنا!' : 'Give Us Your Opinion';
-      document.getElementById('website-btn').textContent =
+      document.getElementById('website-btn').innerHTML =
         lang === 'ar' ? 'اكتشف خدماتنا!' : 'Discover Our Services';
       document.getElementById('label-name').textContent =
         lang === 'ar' ? 'اسم الفني' : 'Technician Name';
       document.getElementById('label-dept').textContent =
-        lang === 'ar' ? 'القسم' : 'Department';
+        lang === 'ar' ? 'القسم' : 'Department';      
+      document.getElementById('label-status').textContent =
+        lang === 'ar' ? 'الحالة' : 'Status';
+      document.getElementById('label-status').textContent =
+        lang === 'ar' ? 'الحالة' : 'Status';
+
 
       const btn = document.getElementById('lang-btn');
       btn.textContent = lang === 'ar' ? 'English' : 'العربية';
